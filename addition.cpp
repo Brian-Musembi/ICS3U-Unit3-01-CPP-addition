9cc2c28a3aa2f97d938a2705a75d3b603a053a21
Brian Musembi <brian.musembi@mths.ca>
2021-04-29 18:31:56 +0000

    added addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 29 April 2021
+// This program adds two numbers together
+
+#include <iostream>
+
+int main() {
+    // this function adds two numbers together
+    float num1;
+    float num2;
+    float sum;
+
+    // input
+    std::cout << "Please enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Please enter the second number: ";
+    std::cin >> num2;
+
+    // process
+    sum = num1 + num2;
+
+    // output
+    std::cout << "" << std::endl;
+    sum = num1 + num2;
+    std::cout << "The answer to " << num1 << " + " << num2 << " is " << sum;
+    return 0;}
